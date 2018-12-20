5d8294a38cb2e0bcd711b440a1b0b415f3730533
Loïc Descotte <loic.descotte@gmail.com>
2018-12-20 17:01:27 +0100

    fix union

--- hamsters-union-types/src/main/scala/io/github/hamsters/Union.scala
+++ hamsters-union-types/src/main/scala/io/github/hamsters/Union.scala
@@ -5,6 +5,7 @@ import scala.collection.immutable.Seq
 import scala.meta.Term.Name
 import scala.meta.{Term, _}
 
+/*
 class UnionMacro extends scala.annotation.StaticAnnotation {
 
   inline def apply(defn: Any): Any = meta {
@@ -44,6 +45,8 @@ class UnionMacro extends scala.annotation.StaticAnnotation {
     }
   }
 }
+
+*/
 
 private object Union {
   implicit class NoEraseInstanceOf(that: Any) {
@@ -98,20 +101,89 @@ case class Union6[T1, T2, T3, T4, T5, T6](v1: Option[T1], v2: Option[T2], v3: Op
 
 case class Union7[T1, T2, T3, T4, T5, T6, T7](v1: Option[T1], v2: Option[T2], v3: Option[T3], v4: Option[T4], v5: Option[T5], v6 : Option[T6], v7 : Option[T7]) extends Union
 
-@UnionMacro
-class Union2Type[T1, T2]
+class Union2Type[T1, T2] {
+  implicit def toUnion21(t: T1)(implicit m1: Manifest[T1], m2: Manifest[T2]) = new Union2[T1, T2](Some(t), None)
 
-@UnionMacro
-class Union3Type[T1, T2, T3]
+  implicit def toUnion22(t: T2)(implicit m1: Manifest[T1], m2: Manifest[T2]) = new Union2[T1, T2](None, Some(t))
+}
 
-@UnionMacro
-class Union4Type[T1, T2, T3, T4]
+class Union3Type[T1, T2, T3] {
+  implicit def toUnion31(t: T1) = new Union3[T1, T2, T3](Some(t), None, None)
 
-@UnionMacro
-class Union5Type[T1, T2, T3, T4, T5]
+  implicit def toUnion32(t: T2) = new Union3[T1, T2, T3](None, Some(t), None)
 
-@UnionMacro
-class Union6Type[T1, T2, T3, T4, T5, T6]
+  implicit def toUnion33(t: T3) = new Union3[T1, T2, T3](None, None, Some(t))
+}
 
-@UnionMacro
-class Union7Type[T1, T2, T3, T4, T5, T6, T7]
+class Union4Type[T1, T2, T3, T4] {
+  implicit def toUnion41(t: T1) = new Union4[T1, T2, T3, T4](Some(t), None, None, None)
+
+  implicit def toUnion42(t: T2) = new Union4[T1, T2, T3, T4](None, Some(t), None, None)
+
+  implicit def toUnion43(t: T3) = new Union4[T1, T2, T3, T4](None, None, Some(t), None)
+
+  implicit def toUnion44(t: T4) = new Union4[T1, T2, T3, T4](None, None, None, Some(t))
+
+}
+
+class Union5Type[T1, T2, T3, T4, T5] {
+  implicit def toUnion51(t: T1) = new Union5[T1, T2, T3, T4, T5](Some(t), None, None, None, None)
+
+  implicit def toUnion52(t: T2) = new Union5[T1, T2, T3, T4, T5](None, Some(t), None, None, None)
+
+  implicit def toUnion53(t: T3) = new Union5[T1, T2, T3, T4, T5](None, None, Some(t), None, None)
+
+  implicit def toUnion54(t: T4) = new Union5[T1, T2, T3, T4, T5](None, None, None, Some(t), None)
+
+  implicit def toUnion55(t: T5) = new Union5[T1, T2, T3, T4, T5](None, None, None, None, Some(t))
+}
+
+/*
+class UnionMacro extends scala.annotation.StaticAnnotation {
+
+  inline def apply(defn: Any): Any = meta {
+
+    def argByXY(idx: Int, i: Int): Term = if (idx == i) {
+      q"""Some(t)"""
+    }
+    else {
+      q"""None"""
+    }
+
+    defn match {
+      case q"class $className[..$classTypes] { ..$body }" =>
+        val types = classTypes.map(t => Type.Name(t.name.value))
+        val typesName: Seq[Name] = classTypes.map(t => Term.Name(t.name.value))
+        val result =
+          q"""
+        class $className[..$classTypes] {
+        ..$body
+         ..${
+
+            typesName.zipWithIndex.map { case (currentTypeName, y) =>
+              val unionXType2UnionXMethodName = Term.Name("toUnion" + y)
+              val currentType = Type.Name(currentTypeName.value)
+              val unionXType = Term.Name("Union" + classTypes.size)
+              val constructorArgs : Seq[Term.Arg] = for (x <- typesName.indices) yield q"""${argByXY(x, y)}"""
+              q"implicit def $unionXType2UnionXMethodName(t : $currentType) = $unionXType[..$types](..$constructorArgs)"
+            }
+          }
+        }
+          """
+        //println(result.syntax) display code result
+        result
+      case _ =>
+        println(defn.structure)
+        abort("@UnionMacroMeta must annotate a class")
+    }
+  }
+}
+
+*/
+
+//@UnionMacro
+//class Union6Type[T1, T2, T3, T4, T5, T6]
+
+//@UnionMacro
+//class Union7Type[T1, T2, T3, T4, T5, T6, T7]
+
